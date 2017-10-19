90047c55eb9171b03cc4433b4ca99f790be6d0c7
boybutcher <BYRONdeLARGE@yahoo.com>
2017-10-18 17:24:21 -0700

    boiler plate server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  // res.statusCode = 200;
+  res.end(`Server is connected!`)
+})
+
+server.listen(3001, 'localhost', () => {
+  console.log(`server.js is listening in on port 3001`)
+})
